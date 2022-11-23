f3ffa7d6d7bf9c78ab9673b224cbd96b2f4b0198
Karel Diaz <kdiazalfonso94@gmail.com>
2022-11-22 23:14:21 -0500

    base

---
+++ src/components/Logo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col items-center space-y-2 relative">
+    <div
+      class="text-white font-bold text-4xl md:text-6xl bg-gradient-to-b shadow-lg from-purple-400 to-purple-600 px-10 py-5 rounded-3xl"
+    >Jorgelina</div>
+    <div
+      class="bg-gradient-to-b text-sm md:text-base absolute -bottom-0 transform -rotate-12 shadow-xl -right-5 from-yellow-400 to-yellow-600 text-white px-3 text-center rounded-md"
+    >CANASTILLAS</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
