446cce689a7d85d96612b58aa25824792d3a58f5
Karel Diaz <kdiazalfonso94@gmail.com>
2022-11-23 13:29:05 -0500

    seo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="relative flex flex-col items-center space-y-2">
+  <h1 class="relative flex flex-col items-center space-y-2">
     <div
       class="px-10 py-5 text-4xl font-bold text-white shadow-lg md:text-6xl bg-gradient-to-b from-purple-400 to-purple-600 rounded-3xl"
     >Jorgelina</div>
     <div
       class="absolute px-3 text-sm text-center text-white transform rounded-md shadow-xl bg-gradient-to-b md:text-base -bottom-0 -rotate-12 -right-5 from-yellow-300 to-yellow-500"
     >CANASTILLA</div>
-  </div>
+  </h1>
 </template>
 
 <script>
