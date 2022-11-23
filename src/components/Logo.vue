30c15168ceae80082a5fa16b242f793112055820
Karel Diaz <kdiazalfonso94@gmail.com>
2022-11-22 23:55:39 -0500

    update

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -4,7 +4,7 @@
       class="px-10 py-5 text-4xl font-bold text-white shadow-lg md:text-6xl bg-gradient-to-b from-purple-400 to-purple-600 rounded-3xl"
     >Jorgelina</div>
     <div
-      class="absolute px-3 text-sm text-center text-white transform rounded-md shadow-xl bg-gradient-to-b md:text-base -bottom-0 -rotate-12 -right-5 from-yellow-400 to-yellow-600"
+      class="absolute px-3 text-sm text-center text-white transform rounded-md shadow-xl bg-gradient-to-b md:text-base -bottom-0 -rotate-12 -right-5 from-yellow-300 to-yellow-500"
     >CANASTILLA</div>
   </div>
 </template>
